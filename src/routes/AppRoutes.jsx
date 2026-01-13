c3dd4fd1d8054f45018b1ad1ee53b6af135e4e45
Gayuth-W <gayuthdemika@gmail.com>
2026-01-13 10:56:18 +0530

    fix: import issue

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Landing from "../pages/Landing";
 import Game from "../pages/Game";
 import Result from "../pages/Result";
