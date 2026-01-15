51b00ffd6b5c7fec4e27371aea5849d52470865c
Gayuth-W <gayuthdemika@gmail.com>
2026-01-15 13:36:46 +0530

    feat: signature

---
+++ src/components/Tag.jsx
@@ -0,0 +1,13 @@
+import "../styles/component.css"
+
+function Tag(){
+  return(
+    <>
+      <div className="tag">
+        <p>Developed By Gayuth Waidyaratne</p>
+      </div>
+    </>
+  );
+}
+
+export default Tag;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,14 +2,15 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Landing from "../pages/Landing";
 import Game from "../pages/Game";
 import Result from "../pages/Result";
+import Tag from "../components/tag";
 
 function AppRoutes(){
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing/>}/>
+        <Route path="/" element={<><Landing/> <Tag/></>}/>
         <Route path="/game" element={<Game/>}/>
-        <Route path="/result" element={<Result/>}/>
+        <Route path="/result" element={<><Result/> <Tag/></>}/>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
