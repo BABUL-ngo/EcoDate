52486bd0452008af218e83fdefb97c390b084a40
Gayuth-W <gayuthdemika@gmail.com>
2026-01-15 13:46:56 +0530

    fix: signature

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Landing from "../pages/Landing";
 import Game from "../pages/Game";
 import Result from "../pages/Result";
-import Tag from "../components/tag";
+import Tag from "../components/Tag";
 
 function AppRoutes(){
   return(
